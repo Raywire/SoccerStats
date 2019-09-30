d4528f79845f7548752f9872e22299cc31a2999a
Raywire <ryanwire@outlook.com>
2019-10-01 00:02:17 +0300

    Initial Setup

---
+++ SoccerStats/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace SoccerStats
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            DirectoryInfo directory = new DirectoryInfo(currentDirectory);
+        }
+    }
+}
