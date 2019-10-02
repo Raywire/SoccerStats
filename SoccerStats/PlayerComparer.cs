fa1e702210fa3c0805963ad35b2ae7612b2b705d
Raywire <ryanwire@outlook.com>
2019-10-02 13:42:39 +0300

    Top Ten Scorers - Determines the top ten scoring players using List.Sort and the IComparer interface

---
+++ SoccerStats/PlayerComparer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoccerStats
+{
+    public class PlayerComparer : IComparer<Player>
+    {
+        public int Compare(Player x, Player y)
+        {
+            // the -1 sorts by descending order
+            return -1 * x.PointsPerGame.CompareTo(y.PointsPerGame);
+        }
+    }
+}
--- SoccerStats/Program.cs
+++ SoccerStats/Program.cs
@@ -15,10 +15,11 @@ namespace SoccerStats
             var fileContents = ReadSoccerResults(fileName);
             fileName = Path.Combine(directory.FullName, "players.json");
             var players = DeserializePlayers(fileName);
+            var topTenPlayers = GetTopTenPlayers(players);
 
-            foreach (var player in players)
+            foreach (var player in topTenPlayers)
             {
-                Console.WriteLine(player.FirstName);
+                Console.WriteLine($"Name: {player.FirstName} PPG: {player.PointsPerGame}");
             }
 
         }
@@ -97,6 +98,21 @@ namespace SoccerStats
             }
             
             return players;
+        }
+
+        public static List<Player> GetTopTenPlayers(List<Player> players)
+        {
+            var topTenPlayers = new List<Player>();
+            players.Sort(new PlayerComparer());
+            int counter = 0;
+            foreach (var player in players)
+            {
+                topTenPlayers.Add(player);
+                counter++;
+                if (counter == 10)
+                    break;
+            }
+            return topTenPlayers;
         }
     }
 }
